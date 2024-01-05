2a8d51536eaf6660a3b06a12f36bc18c9827da29
Andrey Basov <dev.basov@gmail.com>
2024-01-05 04:11:45 +0400

    Added camera-module-load service

---
+++ camera-module-load.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+  then
+    echo "No divice file provided"
+    exit 1
+fi
+
+while true
+do
+  if [ -e "$1" ];
+    then
+      modprobe "$2"
+      break
+    else
+      sleep 0.1
+    fi
+done
